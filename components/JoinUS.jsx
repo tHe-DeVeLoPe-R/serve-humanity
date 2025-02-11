7148f4a1d86d3ad3dc5894d37fd7551c7ab3a79b
saad zafar <codewithsaadzafar@gmail.com>
2025-02-11 10:20:44 +0500

    mobile setting

--- components/JoinUS.jsx
+++ components/JoinUS.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from '../styles/Join.module.css'
 import logo from '../public/images/logo.png'
+import { useState, useEffect } from 'react';
 
 export default function JoinUS() {
-
+   const [isMobile, setIsMobile] = useState(false);
+       
+           useEffect(()=>{
+               setIsMobile(window.innerWidth <= 650)
+           })
   const openWhatsApp = ()=>{
     const phoneNumber = '923097852424';
     const message = "I want to join you as volunteer...";
@@ -13,9 +18,9 @@ export default function JoinUS() {
   }
   return (
     <div className = {styles.joinMain}>
-      <div className = {styles.joinContent}>
+      <div className = {`${isMobile ? styles.mobile_joinContent : styles.joinContent}`}>
         <img src={logo.src} alt="logo" /> <br />
-      Join Serve Humanity as a volunteer and make a meaningful impact! We assist low-income students, provide food supplies to families in need, and support individuals facing health challenges. Your time and dedication can bring hope and relief through event organization, supply distribution, and then raising awareness. Together, let’s foster kindness and create a caring society. Join us today!
+     <p> Join Serve Humanity as a volunteer and make a meaningful impact! We assist low-income students, provide food supplies to families in need, and support individuals facing health challenges. Your time and dedication can bring hope and relief through event organization, supply distribution, and then raising awareness. Together, let’s foster kindness and create a caring society. Join us today!</p>
 
       <button onClick={openWhatsApp}>Connect Our Member To Proceed</button>
       </div>
