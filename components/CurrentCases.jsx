1f2958bfb08c84bfe4241972ef70a7f26480e62a
muhammadfarooq89712 <muhammad.ali@gmail.com>
2025-03-23 23:29:12 +0500

    Resolved merge conflicts in currentCases.jsx

--- components/CurrentCases.jsx
+++ components/CurrentCases.jsx
@@ -16,6 +16,7 @@ export default function CurrentCases() {
          "title": "Medical case",
          "description": "A patient from Lahore is suffering from stage 4 Cancer and is in a critical condition. Unfortunately, they are unable to bear the expensive treatment costs. Please donate generously to support this noble cause. ",
          "AmountRequired": "50k",
+         "AmountCollected": "10k",
          "Deadline": "30 March",
          "Account1": "03196092861 (Abdul Rehman) Jazzcash Account",
          "Account2": "03006844332 (Muhammad Junaid Rashid) Easypaisa Account",
